3169f78fec3a879add3352a01f7c2158d73f4e73
Adam Kuzikov <kuzikov.dev@protonmail.com>
2022-06-03 21:49:34 +0600

    implementation concurrent-safe cache

--- cache.go
+++ cache.go
@@ -1,23 +1,97 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
+// TODO: clear expired entries
+type storage struct {
+	val    string
+	expire *time.Time
+}
+
+// Cache contain concurrent-safe storage for key-value pairs.
 type Cache struct {
+	mu   sync.Mutex
+	data map[string]storage
 }
 
+// NewCache create initialized empty Cache.
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		mu:   sync.Mutex{},
+		data: make(map[string]storage),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+// Get return data
+func (cache *Cache) Get(key string) (string, bool) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	storage, ok := cache.data[key]
+	if !ok {
+		return "", false
+	}
 
+	switch storage.expire {
+	case &time.Time{}:
+		t := storage.expire.Sub(time.Now())
+		if t > time.Nanosecond {
+			return storage.val, true
+		}
+	case nil:
+		return storage.val, true
+	}
+
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+// Put ...
+func (cache *Cache) Put(key, value string) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	cache.data[key] = storage{
+		val:    value,
+		expire: nil,
+	}
+	return
 }
 
-func (receiver) Keys() []string {
+// Keys ...
+func (cache *Cache) Keys() []string {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	keys := make([]string, 0, len(cache.data))
+	now := time.Now()
+	for key, entry := range cache.data {
+
+		if entry.expire == nil || entry.expire.Sub(now) > time.Nanosecond {
+			keys = append(keys, key)
+			continue
+		}
+
+		// notExpired :=
+		// if notExpired {
+		// 	keys = append(keys, key)
+		// 	continue
+		// }
+
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+// PutTill ...
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	timeCopy := deadline
+	cache.data[key] = storage{
+		val:    value,
+		expire: &timeCopy,
+	}
+	return
+
 }
